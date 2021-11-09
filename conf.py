aea4fe35aa4ab239f5546e9ae0dcd67a55d8e233
apg <znjsapg@apg.cn>
2021-11-09 16:30:58 +0800

    furo 主题

--- conf.py
+++ conf.py
@@ -60,7 +60,7 @@ exclude_patterns += [p.as_posix() for p in Path('sandbox').iterdir() if p.is_dir
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'furo'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
